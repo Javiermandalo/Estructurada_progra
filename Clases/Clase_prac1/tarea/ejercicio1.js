48d55e9d9c10146aeef2191939b0d9af250a13d1
Javier Flores <javierflores2367@gmail.com>
2026-08-26 17:43:28 -0600

    sbirr

--- Clases/Clase_prac1/tarea/ejercicio1.js
+++ Clases/Clase_prac1/tarea/ejercicio1.js
@@ -1,17 +1,11 @@
-// Declaramos la variable 'radio' y le asignamos un valor numérico
+
 let radio = 2.5;
 
-// Calculamos el perímetro (circunferencia) usando la fórmula: 2 * pi * radio
-let perimetro = 2 * (3.14159) * radio;
 
-// Calculamos el área usando la fórmula: pi * radio al cuadrado (radio ** 2)
+let perimetro = 2 * (3.14159) * radio;
 let area = (3.14159) * radio ** 2;
 
-// Mostramos en la consola el valor del radio
+
 console.log("Radio: " + radio);
-
-// Mostramos en la consola el perímetro, redondeado a dos decimales con .toFixed(2)
 console.log("Perimetro: " + perimetro.toFixed(2));
-
-// Mostramos en la consola el área, también redondeada a dos decimales
 console.log("Area: " + area.toFixed(2));
--- Clases/Clase_prac1/tarea/ejercicio3.js
+++ Clases/Clase_prac1/tarea/ejercicio3.js
@@ -1,26 +1,10 @@
-// Declaramos la cantidad de kilowatts (kWh) consumidos durante el mes
 let kWh = 250;
-
-// Definimos el precio fijo por cada kilowatt (constante porque no cambia)
 const kw = 0.15;
-
-// Calculamos el costo base multiplicando los kilowatts consumidos por el precio unitario
 let costoBase = kWh * kw;
-
-// Calculamos el impuesto correspondiente al 10% del costo base
 let impuesto = costoBase * 0.1;
-
-// Calculamos el costo total sumando el costo base más el impuesto
 let costoTotal = costoBase + impuesto;
 
-// Mostramos en consola la cantidad de kilowatts consumidos
 console.log("Consumo: " + kWh  + " kWh");
-
-// Mostramos el costo base sin impuestos, redondeado a dos decimales
 console.log("Costo sin impuesto: $" + costoBase.toFixed(2));
-
-// Mostramos el monto del impuesto (10%), redondeado a dos decimales
 console.log("Impuesto (10%): $" + impuesto.toFixed(2));
-
-// Mostramos el costo total a pagar, redondeado a dos decimales
 console.log("Costo total a pagar: $" + costoTotal.toFixed(2));
